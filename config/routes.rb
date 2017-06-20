e79ca2442ba2a5b4747ddbacf4ae3e61a8a19665
lucyzoo <lucyzhu12@hotmail.com>
2017-06-20 10:13:11 -0700

    adding tab and link to new home page post login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get '/home', to:'static_pages#homepostlogin', as: 'homepostlogin'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   resources :users
