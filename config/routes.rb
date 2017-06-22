80e56a05d3cb90d42d4cd294dfb106a0a6f51bae
lucyzoo <lucyzhu12@hotmail.com>
2017-06-22 09:43:39 -0700

    logo adjustment + about us/help page edits

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/home', to:'static_pages#homepostlogin', as: 'homepostlogin'
   get '/about', to: 'static_pages#aboutpage', as: 'aboutpage'
-  get 'help', to: 'static_pages#helppage', as: 'helppage'
+  get '/help', to: 'static_pages#helppage', as: 'helppage'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   resources :users
