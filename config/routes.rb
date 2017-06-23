d7ac5212c38847d3b6ec6064edf50faae01dc3a1
Ambar <ambar.dhesi@gmail.com>
2017-06-23 13:45:20 -0700

    Added a TODO

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   root 'static_pages#home'
-  get '/home', to:'static_pages#homepostlogin', as: 'homepostlogin'  
+  # TODO fix home swap slash
+  get '/home', to:'static_pages#homepostlogin', as: 'homepostlogin'
   get '/about', to: 'static_pages#aboutpage', as: 'aboutpage'
   get '/help', to: 'static_pages#helppage', as: 'helppage'
   get  '/signup',  to: 'users#new'
