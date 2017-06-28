7951041cd31651210ff6e5f8e4fc0ff3ac95ec90
Alexis Lizardo <alizardo@sfu.ca>
2017-06-28 10:10:37 -0700

    Add tests for user delete

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -32,13 +32,28 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should not allow the admin attribute to be edited via the web" do
-    log_in_as(@other_user)
-    assert_not @other_user.admin?
-    patch user_path(@other_user), params: {
+    log_in_as(@user)
+    assert_not @user.admin?
+    patch user_path(@user), params: {
                                     user: { password:              'password',
                                             password_confirmation: 'password',
                                             admin: true } }
-    assert_not @other_user.reload.admin?
+    assert_not @user.reload.admin?
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@user)
+    assert_no_difference 'User.count' do
+      delete user_path(@other_user)
+    end
+    assert_redirected_to root_url
   end
 
 end
