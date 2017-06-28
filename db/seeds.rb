0a7d92a833fbac5e16103c8b0d79dbc787a02a2d
Alexis Lizardo <alizardo@sfu.ca>
2017-06-28 10:02:37 -0700

    Add admin attribute and delete user functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,12 @@
 User.create!(name:  "test",
              email: "test@test.com",
              password:              "1234567",
-             password_confirmation: "1234567")
+             password_confirmation: "1234567",
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+  email = "example-#{n+1}@test.com"
   password = "password"
   User.create!(name:  name,
                email: email,
