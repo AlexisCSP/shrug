b6a6949594c98affa1d0338b6041ce9c84049528
Ambar <ambar.dhesi@gmail.com>
2017-07-12 22:11:54 -0700

    Migrating database to save user location and latitude

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170628164233) do
+ActiveRecord::Schema.define(version: 20170713051107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
