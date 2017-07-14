c1ed1c43aefdb7ba0617983e623db7071ad4cb9c
Mayank Aggarwal <maggarwal368@gmail.com>
2017-07-14 13:47:01 -0700

    Uploading pics

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	mount_uploader :image, ImageUploader
 	attr_accessor :remember_token
 	before_save { email.downcase! }
 	validates :name, presence: true, length: { maximum: 50 }
