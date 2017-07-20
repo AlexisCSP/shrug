f729bba6b532bf528495ff8d03405932db5559c6
Alexis Lizardo <alizardo@sfu.ca>
2017-07-20 14:26:30 -0700

    Add account activation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token
-	before_save { email.downcase! }
+  attr_accessor :remember_token, :activation_token
+  before_save { email.downcase! }
+  before_create :create_activation_digest
+
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -47,4 +49,13 @@ class User < ApplicationRecord
     end
     existing_chat_room_users.uniq
   end
+
+  private
+
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
+
 end
