b2987034b5cd92174237835cc5c3a02e538c2307
Alexis Lizardo <alizardo@sfu.ca>
2017-07-20 16:35:56 -0700

    Add forgot password, and send email with reset link

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save { email.downcase! }
   before_create :create_activation_digest
 
@@ -61,6 +61,15 @@ class User < ApplicationRecord
 
   def update_coord(lat, long)
     update_columns(latitude: lat, longitude: long)
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
 
   private
