5bf2720132c0ca58eb9e25bcefff49620701a77d
Mayank Aggarwal <maggarwal368@gmail.com>
2017-07-21 12:58:13 -0700

    Small fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+                                   :password_confirmation, :image)
     end
 
     def logged_in_user
