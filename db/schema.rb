a34f1acbe4091e7893c92fae91b1b16ebc3bfe54
Ambar <ambar.dhesi@gmail.com>
2017-07-20 20:03:50 -0700

    Added lat lng to chat_room model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170720231650) do
+ActiveRecord::Schema.define(version: 20170721030254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20170720231650) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "latitude", precision: 10, scale: 6
+    t.decimal "longitude", precision: 10, scale: 6
     t.index ["user_id"], name: "index_chat_rooms_on_user_id"
   end
 
@@ -48,7 +50,7 @@ ActiveRecord::Schema.define(version: 20170720231650) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
-    t.boolean "admin", default: false
+    t.boolean "admin"
     t.decimal "latitude", precision: 10, scale: 6
     t.decimal "longitude", precision: 10, scale: 6
     t.string "activation_digest"
