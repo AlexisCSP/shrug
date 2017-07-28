2c6a21ac8bb757889df13e673b2d10a49403eaa4
Ambar <ambar.dhesi@gmail.com>
2017-07-28 13:11:32 -0700

    Added thumbnails for users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,35 @@
 module UsersHelper
-  def get_img_path
-    if current_user.image.blank?
-      user_image = asset_path("default.jpg")
+  def get_img_path(user)
+    if user.image.blank?
+      user_image = image_path("default.jpg")
     else
-      user_image = asset_path(current_user.image)
+      user_image = image_path(user.image)
       def_image = user_image.split('/')
-      def_image = user_image.last()
-      if def_image == "default.jpg"
-        user_image = asset_path(def_image)
+      image_name = def_image.last()
+      image_name = image_name
+      if image_name == "default.jpg"
+        user_image = image_path(image_name)
+      else
+        def_image[-1] = image_name
+        user_image = def_image.join('/')
+      end
+    end
+    return user_image
+  end
+
+  def get_thumb_img_path(user)
+    if user.image.blank?
+      user_image = image_path("thumb_default.jpg")
+    else
+      user_image = image_path(user.image)
+      def_image = user_image.split('/')
+      image_name = def_image.last()
+      image_name = "thumb_" + image_name
+      if image_name == "thumb_default.jpg"
+        user_image = image_path(image_name)
+      else
+        def_image[-1] = image_name
+        user_image = def_image.join('/')
       end
     end
     return user_image
